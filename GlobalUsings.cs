0376e0ad8ff5ebdcb6b5d5a5dda6e30ef55e9be7
Salma magdi <110284610+salma-magdi@users.noreply.github.com>
2025-06-27 10:41:45 +0300

    Add files via upload

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,7 +1,10 @@
 ﻿global using BookShoppingCartMvcUI.Models;
+global using BookShoppingCartMvcUI.Models.DTOS;
 global using BookShoppingCartMvcUI.Data;
 global using BookShoppingCartMvcUI.Repositories;
 global using BookShoppingCartMvcUI.Controllers;
 global using BookShoppingCartMvcUI.Constants;
 global using BookShoppingCartMvcUI.Areas;
+
+
 
--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 using System;
 using BookShoppingCartMvcUI.Data;
 using BookShoppingCartMvcUI.Models;
+using BookShoppingCartMvcUI.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 
 namespace BookShoppingCartMvcUI
 {
@@ -14,62 +14,42 @@ namespace BookShoppingCartMvcUI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            
+
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-            if (string.IsNullOrEmpty(connectionString))
-            {
-                throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            }
-
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
+                    options.UseSqlServer(connectionString));
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
-                .AddRoles<IdentityRole>()
+            builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultUI()
                 .AddDefaultTokenProviders();
 
             builder.Services.AddControllersWithViews();
             builder.Services.AddTransient<IHomeRepository, HomeRepository>();
-            builder.Services.AddTransient<DbSeeder>(); //  ”ÃÌ· DbSeeder
+            builder.Services.AddTransient<ICartRepository, CartRepository>();
+
 
             var app = builder.Build();
-
-            // Seed default data
-            using (var scope = app.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                var logger = services.GetService<ILogger<Program>>();
-                var dbSeeder = services.GetService<DbSeeder>();
-                try
-                {
-                    if (dbSeeder != null)
-                    {
-                        await dbSeeder.SeedDefaultData();
-                    }
-                }
-                catch (Exception ex)
-                {
-                    logger?.LogError(ex, "An error occurred while seeding the database.");
-                }
-            }
+            //using (var scope = app.Services.CreateScope())
+            //{
+            //    await DbSeeder.SeedDefaultData(scope.ServiceProvider);
+            //}
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
             }
-
             app.UseStaticFiles();
+
             app.UseRouting();
-            app.UseAuthentication();
+
             app.UseAuthorization();
 
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
-            app.MapRazorPages();
 
             app.Run();
         }
